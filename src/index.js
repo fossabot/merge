52fcf71b3b73325afe1bdab14358c9dd56cebd0a
blackcater <blackcater2015@gmail.com>
2018-02-26 16:38:54 +0800

    Refactoring `merge.js`

--- src/index.js
+++ src/index.js
@@ -3,110 +3,127 @@ import * as util from './util'
 /* eslint-disable */
 
 /**
- * 数组合并
+ * merge source and target
  *
- * @param source {Array}
- * @param target {Any}
- * @param options {Object}
+ * @param options
  */
-function mergeArray(source, target, options = {}) {
-  const opts = {
-    // push | unshift | merge | replace
-    type: 'replace',
-    // 是否强制 target 为数组
-    array: true,
-    // 是否去除共有元素
-    filter: true,
-    // 是否对子元素进行 merge 操作
-    merge: false,
-    // null 值是否有意义
-    nullable: true,
-    // undefined 是否有意义
-    undefinable: true,
-    ...options,
-  }
-
-  // 强制 target 为 array 对象
-  if (opts.array && !util.isArray(target)) {
-    return target
-  }
-
-  // replace 替换
-  if (opts.type === 'replace') {
-    return target
-  }
-
-  const targetList = util.isArray(target) ? target : [target]
-  const list = source.concat([])
-  const temp = []
-
-  for (let i = 0, len = targetList.length; i < len; i++) {
-    const index = list.indexOf(targetList[i])
-
-    if (opts.type === 'push' || opts.type === 'unshift') {
-      // 移除共有元素
-      if (opts.filter && index !== -1) {
-        list.splice(index, 1)
-      } else {
-        temp.push(targetList[i])
-      }
-    } else if (opts.type === 'merge') {
-      if (opts.merge) {
-        list[i] = merge(
-          list[i],
-          targetList[i],
-          util.isObject(opts.merge) ? opts.merge : {}
-        )
-      } else if (
-        opts.nullable ||
-        targetList[i] !== null ||
-        (opts.undefinable || targetList[i] !== undefined)
-      ) {
-        list[i] = targetList[i]
-      }
+export default class Merge {
+  constructor(options = {}) {
+    this.options = {
+      array: {
+        // push | unshift | merge | replace
+        type: 'replace',
+        // 是否强制 target 为数组
+        array: true,
+        // 是否去除共有元素
+        filter: true,
+        // 是否对子元素进行 merge 操作
+        merge: false,
+        // null 值是否有意义
+        nullable: true,
+        // undefined 是否有意义
+        undefinable: true,
+        ...(options.array || {}),
+      },
+      object: {
+        // 强制 target 为对象
+        object: true,
+        // null 值是否有意义
+        nullable: true,
+        // undefined 是否有意义
+        undefinable: true,
+        ...(options.object || {}),
+      },
     }
   }
 
-  if (temp.length > 0) {
-    return opts.type === 'unshift' ? temp.concat(list) : list.concat(temp)
-  } else {
-    return list
-  }
-}
+  merge(source, target) {
+    // 数组合并
+    if (util.isArray(source)) {
+      return this.mergeArray(source, target, this.options.array)
+    }
 
-/**
- * 对象合并
- *
- * @param source
- * @param target
- * @param options
- */
-function mergeObject(source, target, options = {}) {
-  const opts = {
-    // null 值是否有意义
-    nullable: true,
-    // undefined 是否有意义
-    undefinable: true,
-  }
-}
+    // 对象合并
+    if (util.isObject(source)) {
+      return this.mergeObject(source, target, this.options.object)
+    }
 
-/**
- * merge source and target
- *
- * @param source
- * @param target
- * @param options
- */
-export default function merge(source, target, options = {}) {
-  // 数组合并
-  if (util.isArray(source)) {
-    return mergeArray(source, target, options)
+    return target
   }
 
-  // 对象合并
-  if (util.isObject(source)) {
-    return mergeObject(source, target, options)
+  /**
+   * 数组合并
+   *
+   * @param source {Array}
+   * @param target {Any}
+   * @param options {Object}
+   */
+  mergeArray(source, target, options = {}) {
+    // 强制 target 为 array
+    if (options.array && !util.isArray(target)) {
+      return target
+    }
+
+    // replace 替换
+    if (options.type === 'replace') {
+      return target
+    }
+
+    const targetList = util.isArray(target) ? target : [target]
+    const list = [...source]
+    const temp = []
+
+    for (let i = 0, len = targetList.length; i < len; i++) {
+      const index = list.indexOf(targetList[i])
+
+      if (options.type === 'push' || options.type === 'unshift') {
+        // 移除共有元素
+        if (options.filter && index !== -1) {
+          list.splice(index, 1)
+        } else {
+          temp.push(targetList[i])
+        }
+      } else if (options.type === 'merge') {
+        if (options.merge) {
+          list[i] = merge(
+            list[i],
+            targetList[i],
+            util.isObject(options.merge) ? options.merge : {}
+          )
+        } else if (
+          options.nullable ||
+          targetList[i] !== null ||
+          (options.undefinable || targetList[i] !== undefined)
+        ) {
+          list[i] = targetList[i]
+        }
+      }
+    }
+
+    if (temp.length > 0) {
+      return options.type === 'unshift' ? temp.concat(list) : list.concat(temp)
+    } else {
+      return list
+    }
   }
 
-  return target
+  /**
+   * 对象合并
+   *
+   * @param source
+   * @param target
+   * @param options
+   */
+  mergeObject(source, target, options = {}) {
+    // 强制 target 为 object
+    if (options.object && !util.isObject(target)) {
+      return target
+    }
+
+    const result = { ...source }
+    const targetObject = Object.assign({}, target)
+
+    for (const key in targetObject) {
+    }
+  }
 }
